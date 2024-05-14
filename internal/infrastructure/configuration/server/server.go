f2f090d3908b9cd76a59e983509fe3fcd476101e
Esteban Andrey Gomez A <79619416+unawaretub86@users.noreply.github.com>
2024-05-13 21:41:42 -0500

    Develop (#5)
    * initial-scaffolding (#1)
    
    * initial-scaffolding
    
    * real initial scaffolding
    
    * adding workflows
    
    * change db name users
    
    * Adding initial tables
    
    * Fixing fields names
    
    * Fixing some issues
    
    * adding pr template (#3)
    
    * adding pr template
    
    * order md
    
    * Modifying DDL scripts for create table names in capitalcase
    
    * Fixing table names
    
    ---------
    
    Co-authored-by: Sebastian Valencia <sebastianvalencia.ceron@gmail.com>

---
+++ internal/infrastructure/configuration/server/server.go
@@ -0,0 +1,21 @@
+package server
+
+import (
+	"github.com/gin-gonic/gin"
+
+	"github.com/unawaretub86/MoneyTracker/internal/infrastructure/dependencies"
+	"github.com/unawaretub86/MoneyTracker/internal/infrastructure/roles/read"
+	"github.com/unawaretub86/MoneyTracker/internal/infrastructure/roles/write"
+)
+
+func NewServer(basePath string, container *dependencies.Container) {
+	r := gin.Default()
+
+	read.NewRead(container).RegisterRoutes(basePath, r)
+	write.NewWrite(container).RegisterRoutes(basePath, r)
+
+	err := r.Run()
+	if err != nil {
+		panic(err.Error())
+	}
+}
